7868217aee6d1f420ce9c67d170326c177149b63
caraanralphy <caraanralph@gmail.com>
2023-12-16 05:25:15 +0800

    5:25AM of 12/16/23

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,15 @@
+import { motion, useAnimate } from "framer-motion";
+
 import teakapLogo from "./assets/logo/teakapLogo.png";
 import Image from "next/image";
-import { useHover } from "./hooks/useHover";
 import { useRef } from "react";
 
 const Navbar = () => {
-  const { scope, linkHover, linkNotHover } = useHover();
+  const [Home, animateHome] = useAnimate();
+  const [About, animateAbout] = useAnimate();
+  const [Menus, animateMenus] = useAnimate();
+  const [Contacts, animateContacts] = useAnimate();
+
   return (
     <div className="grid grid-cols-2 px-7 md:px-20 py-5">
       <div className="flex items-center -space-x-3 text-3xl font-semibold">
@@ -19,47 +24,101 @@ const Navbar = () => {
           <li className="group">
             <a
               onMouseEnter={() => {
-                linkHover("hover-home");
+                animateHome(
+                  ".hover-border",
+                  {width: "100%" },
+                  { type: "tween", duration: 0.1 }
+                );
               }}
               onMouseLeave={() => {
-                linkNotHover("hover-home");
+                animateHome(
+                  ".hover-border",
+                  { width: 0 },
+                  { type: "tween", duration: 0.1 }
+                );
               }}
-              className="focus:none font-bold"
+              className="focus:none font-semibold"
               href="#home"
             >
               Home
             </a>
-            <div className="flex flex-col items-center w-full" ref={scope}>
-              <div className="hover-home w-0 h-[3px] bg-gray-900"></div>
+            <div className="flex flex-col items-center w-full" ref={Home}>
+              <div className="hover-border w-0 h-[3px] bg-gray-900"></div>
             </div>
           </li>
-          <li className="group">
-            <a
+          <li>
+          <a
               onMouseEnter={() => {
-                linkHover("hover-about");
+                animateAbout(
+                  ".hover-about",
+                  {width: "100%" },
+                  { type: "tween", duration: 0.1 }
+                );
               }}
               onMouseLeave={() => {
-                linkNotHover("hover-about");
+                animateAbout(
+                  ".hover-about",
+                  { width: 0 },
+                  { type: "tween", duration: 0.1 }
+                );
               }}
-              className="focus:none font-bold"
+              className="focus:none font-semibold"
               href="#about"
             >
               About
             </a>
-            <div className="flex flex-col items-center w-full" ref={scope}>
+            <div className="flex flex-col items-center w-full" ref={About}>
               <div className="hover-about w-0 h-[3px] bg-gray-900"></div>
             </div>
           </li>
           <li>
-            <a href="#services">Services</a>
-            <div className="w-full">
-              <div className="h-[3px] w-full bg-gray-900"></div>
+          <a
+              onMouseEnter={() => {
+                animateMenus(
+                  ".hover-menus",
+                  {width: "100%" },
+                  { type: "tween", duration: 0.1 }
+                );
+              }}
+              onMouseLeave={() => {
+                animateMenus(
+                  ".hover-menus",
+                  { width: 0 },
+                  { type: "tween", duration: 0.1 }
+                );
+              }}
+              className="focus:none font-semibold"
+              href="#menus"
+            >
+              Menus
+            </a>
+            <div className="flex flex-col items-center w-full" ref={Menus}>
+              <div className="hover-menus w-0 h-[3px] bg-gray-900"></div>
             </div>
           </li>
           <li>
-            <a href="#contact">Contact</a>
-            <div className="w-full">
-              <div className="h-[3px] w-full bg-gray-900"></div>
+          <a
+              onMouseEnter={() => {
+                animateContacts(
+                  ".hover-contacts",
+                  {width: "100%" },
+                  { type: "tween", duration: 0.1 }
+                );
+              }}
+              onMouseLeave={() => {
+                animateContacts(
+                  ".hover-contacts",
+                  { width: 0 },
+                  { type: "tween", duration: 0.1 }
+                );
+              }}
+              className="focus:none font-semibold"
+              href="#contacts"
+            >
+              Contacts
+            </a>
+            <div className="flex flex-col items-center w-full" ref={Contacts}>
+              <div className="hover-contacts w-0 h-[3px] bg-gray-900"></div>
             </div>
           </li>
         </ul>
